155f538e6f3fd642bf057c4b7ce7f9cba83d690b
BogdanSharaiev <114177706+BogdanSharaiev@users.noreply.github.com>
2025-05-14 15:10:49 +0300

    Delete registration directory

--- registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    path('login/', LoginView.as_view()),
-    path('register/', RegisterView.as_view()),
-    path('refresh/', RegisterView.as_view())
-]
