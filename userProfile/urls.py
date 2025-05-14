6e7cd3185ea88dff02632716e1ae6cb9353045c8
BogdanSharaiev <114177706+BogdanSharaiev@users.noreply.github.com>
2025-05-14 15:10:57 +0300

    Delete userProfile directory

--- userProfile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserProfileView, UpdateUserProfileView, UserProfileView
-
-urlpatterns = [
-    path('info/', UserProfileView.as_view()),
-    path('update/', UpdateUserProfileView.as_view())
-]
